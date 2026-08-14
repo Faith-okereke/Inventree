9c875a4f8802a638ef52b923670e232c6526fbe7
Faith-okereke <faithokereke26@gmail.com>
2026-08-14 11:08:53 +0100

    fix(build):add correct build script for backend

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 
 /**
@@ -13,14 +13,26 @@ import Image from "next/image";
 export default function AuthLayout({ children }: LayoutProps<"/">) {
   return (
     <div className="grid min-h-dvh lg:flex ">
-      <aside className="hidden lg:flex lg:items-center lg:justify-center lg:sticky lg:top-0 lg:h-screen lg:overflow-hidden lg:bg-canvas lg:p-12 lg:max-w-[35%]">
+      <aside className="hidden lg:flex lg:flex-col lg:items-center lg:justify-center lg:sticky lg:top-0 lg:h-screen lg:overflow-hidden lg:bg-canvas lg:p-12 lg:max-w-[35%]">
+        <div className="animate-fade-up relative  mb-10 flex flex-col items-center justify-normal text-center">
+          <h1 className="text-3xl text-white font-bold ">INVENTREE</h1>
+          <p className="text-black"> Know what you have, where it is, and when to reorder</p>
+        </div>
+
         {/* Soft radial wash behind the artwork */}
         <div
           aria-hidden
-          className="absolute inset-0 bg-[radial-gradient(circle_at_30%_25%,rgba(255,255,255,0.28),transparent_60%)]"
+          className=" absolute inset-0 bg-[radial-gradient(circle_at_30%_25%,rgba(255,255,255,0.28),transparent_60%)]"
         />
+
         <div className="animate-fade-up relative ">
-         <Image src="/auth-image.png" alt="Auth Illustration" width={400} height={400} className="relative bottom-0 left-0"/>
+          <Image
+            src="/auth-image.png"
+            alt="Auth Illustration"
+            width={500}
+            height={500}
+            className="relative bottom-0 left-0"
+          />
         </div>
       </aside>
 
