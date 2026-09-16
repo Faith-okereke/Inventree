0b754f15420fbfff28625b1c097f64d94fd9629e
Faith-okereke <faithokereke26@gmail.com>
2026-09-16 10:12:31 +0100

    fix(deploy): deploy issues fixxed

--- frontend/app/(auth)/accept-invitation/page.tsx
+++ frontend/app/(auth)/accept-invitation/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
+import AcceptInvitationForm from "./accept-invitation";
 
 export default function AcceptInvitationPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <AcceptInvitationPage />
+      <AcceptInvitationForm />
     </Suspense>
   );
 }
